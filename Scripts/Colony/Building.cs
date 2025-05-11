c4f1a14fc958934ab188e430aa728e1c82b6e852
Matthew Nock <thematt.nock@gmail.com>
2025-05-11 08:51:41 +0100

    building script base class, to be inherited by buildings

--- Scripts/Colony/Building.cs
+++ Scripts/Colony/Building.cs
@@ -3,13 +3,44 @@ using System;
 
 public partial class Building : Node2D
 {
+	public MeshInstance2D Mesh;
+	public bool placed = false;
+	public Color color = new Color(1, 1, 1);
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		Mesh = new MeshInstance2D();
+		Mesh.Mesh = new SphereMesh();
+		Scale = new Vector2(50, 50);
+		AddChild(Mesh);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		BuildingPlacement();
 	}
+	public void BuildingPlacement()
+	{
+		if (!placed)
+		{
+			Modulate = new Color(color.R, color.G, color.B, 0.5f);
+			GlobalPosition = GetGlobalMousePosition();
+		}
+		else
+		{
+			Modulate = new Color(color.R, color.G, color.B, 1f);
+		}
+	}
+	public override void _Input(InputEvent @event)
+	{
+		if (@event is InputEventMouseButton mouseButtonEvent)
+		{
+			if (mouseButtonEvent.ButtonIndex == MouseButton.Left && mouseButtonEvent.Pressed)
+			{
+				placed = true;
+			}
+		}
+	}
+
 }
