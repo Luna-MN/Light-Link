fd3b4d6c0e94fb4bd056bf5f186e0e5c19aaeac5
Matthew Nock <thematt.nock@gmail.com>
2025-05-25 17:27:33 +0100

    start of a place to take the resources

---
+++ Scripts/Colony/ResourcePlace.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using Godot;
+
+public partial class ResourcePlace : Building
+{
+    public override void _Ready()
+    {
+        base._Ready();
+    }
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
+    }
+}
